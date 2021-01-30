10d3ac8651089b0be0d6212e8c67f3279f7f9ae0
zaidanr <46199073+zaidanr@users.noreply.github.com>
2021-01-30 21:08:02 +0700

    Change class & var name to more generic ones

--- serializer.php
+++ serializer.php
@@ -3,11 +3,11 @@
 // RUN: php serialize.php
 
 // Change Class name to target Class name.
-class DatabaseExport {
+class TargetClass {
 
     // Change variable to target variable.
-    public $user_file = 'gitmasterson1c.html';
-    public $data = '<h1>Testing</h1>';
+    public $target_file = 'gitmasterson1c.html';
+    public $file_content = '<h1>Testing</h1>';
 }
 
 echo "Plain Payload: \n" . serialize(new DatabaseExport) . "\n";
